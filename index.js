c8caa66dba49954dc60b4d19a5c0c124c3bf51e5
Ibezimmichael <ibezim.michael@yahoo.com>
2024-05-12 09:37:54 +0100

    Testing changes

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.get("/api/v1", (req, res) => {
-    res.send("Test ebfw")
+    res.send("Test ebfw !!!")
     console.log("This works");
 })
 app.use('/api/v1/posts', posts);
